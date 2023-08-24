1be98696a71b2d2e70bc6b55fd6fa07759329096
the stray cat <43429810+haughty945@users.noreply.github.com>
2023-08-24 14:11:42 +0800

    添加获取视频流函数

--- src/gros_client/robot.py
+++ src/gros_client/robot.py
@@ -1,32 +1,14 @@
-import asyncio
 import json
 import queue
 import threading
 from concurrent.futures import ThreadPoolExecutor
-from typing import Any, Dict, Callable
+from typing import Any, Dict
 
 import requests
-import websockets
-
 from websocket import *
 
-
-class Mod:
-    MOD_ORIGINAL = 'ORIGINAL'
-    MOD_ACTION = 'ACTION'
-    MOD_HOME = 'HOME'
-    MOD_FIX = 'FIX'
-
-    MOD_4_WHEEL = 'WHEEL_4'
-    MOD_3_WHEEL = 'WHEEL_3'
-    MOD_2_WHEEL = 'WHEEL_2'
-
-
-class RobotType:
-    CAR = 'car'
-    HUMAN = 'human'
-    DOG = 'dog'
-
+from .mod import Mod
+from .robot_type import RobotType
 
 executor = ThreadPoolExecutor()
 
@@ -46,11 +28,7 @@ class Robot:
             self.baseurl = f'http://{host}:8001'
             self.ws_url = f'ws://{host}:8001/ws'
 
-        self.message: queue = None
-        self.websocket: websockets = None
-        # asyncio.get_event_loop().run_until_complete(self._connect())
         self.ws: WebSocket = create_connection(self.ws_url)
-        print(self.ws_url)
         self.receive_thread = threading.Thread(target=self.receive_loop)
         self.receive_thread.start()
 
@@ -60,7 +38,7 @@ class Robot:
             result_json = json.loads(result)
             if "function" in result_json:
                 print("生产：length", self.states_queue.qsize())
-                if (self.states_queue.qsize() > 0):
+                if self.states_queue.qsize() > 0:
                     print("生产:先移除，后添加")
                     self.states_queue.get()
                     self.states_queue.put(result)
@@ -77,18 +55,16 @@ class Robot:
         else:
             return None
 
-    async def connect_to_server(self):
-        async with websockets.connect('ws://localhost:8001/ws') as websocket:
-            self.websocket = await websockets.connect(self.ws_url)
-            if self.on_open:
-                await self.on_open(self.websocket)
-            while True:
-                message = await websocket.recv()
-                print(message)
-
     def get_type(self) -> Dict[str, Any]:
         response = requests.get(f'{self.baseurl}/robot/type')
         return response.json()
+
+    def get_video_status(self) -> Dict[str, Any]:
+        response = requests.get(f'{self.baseurl}/control/camera')
+        return response.json()
+
+    def get_video_stream_url(self) -> str:
+        return f'{self.baseurl}/control/camera'
 
     def set_mode(self, mod: Mod) -> Dict[str, Any]:
         if self.type == RobotType.CAR:
@@ -123,13 +99,13 @@ class Robot:
         else:
             print('robot type not allow this command! The current function is only applicable to humans')
 
-    async def stand(self) -> Dict[str, Any]:
+    def stand(self) -> Dict[str, Any]:
         if self.type == RobotType.HUMAN:
             response = requests.post(f'{self.baseurl}/robot/stand')
             return response.json()
         print('robot type not allow this command! The current function is only applicable to human')
 
-    async def move(self, angle: float, speed: float):
+    def move(self, angle: float, speed: float):
         angle = self._cover_param(angle, 'angle', -45, 45)
         speed = self._cover_param(speed, 'speed', -0.8, 0.8)
         self._send_websocket_msg({
@@ -140,9 +116,9 @@ class Robot:
             }
         })
 
-    async def head(self, roll: float, pitch: float, yaw: float):
+    def head(self, roll: float, pitch: float, yaw: float):
         if self.type == RobotType.HUMAN:
-            await self._send_websocket_msg({
+            self._send_websocket_msg({
                 'command': 'head',
                 'data': {
                     'roll': roll,
@@ -152,8 +128,14 @@ class Robot:
             })
         print('robot type not allow this command! The current function is only applicable to human')
 
-    @staticmethod
-    def _cover_param(param: float, value: str, min_threshold: float, max_threshold: float) -> float:
+    def close_websocket(self):
+        self.ws.close()
+
+    def _send_websocket_msg(self, message: json):
+        self.ws.send(json.dumps(message))
+
+    @classmethod
+    def _cover_param(cls, param: float, value: str, min_threshold: float, max_threshold: float) -> float:
         if param is None:
             print(f"Illegal parameter: {value} = {param}")
             param = 0
@@ -168,9 +150,3 @@ class Robot:
                 f"greater than maximum, expected not to be less than {min_threshold}, actual {param}")
             param = min_threshold
         return param
-
-    def _send_websocket_msg(self, message: json):
-        self.ws.send(json.dumps(message))
-
-    def close_websocket(self):
-        self.ws.close()
---
+++ src/gros_client/robot_type.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class RobotType(Enum):
+
+    CAR = "car",
+    HUMAN = "human",
+    DOG = "dog"
