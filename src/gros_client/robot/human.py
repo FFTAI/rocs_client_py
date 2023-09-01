4667d58aef8af7a80797d053cdb15f0ae943e398
天行者（李朋斌） <pengbin.li@fftai.com>
2023-09-01 10:23:02 +0800

    机器人实时状态字段注释

--- src/gros_client/robot/human.py
+++ src/gros_client/robot/human.py
@@ -165,6 +165,261 @@ class Human(RobotBase):
         Args:
             frequence(int): 频率
 
+        Returns:
+            data (dict): 响应数据
+                - log (dict): 日志信息
+                    - logBuffer (list): 日志缓冲区
+                        - log (str): 日志内容
+                - states (dict): 关节状态数据
+                    - basestate (dict): 机器人状态数据
+                        - a (float): hip roll
+                        - b (float): hip Pitch
+                        - c (float): hip Yaw
+                        - va (float): not use
+                        - vb (float): not use
+                        - vc (float): not use
+                        - vx (float): 前进方向速度，单位m/s
+                        - vy (float): 左右方向速度，单位m/s
+                        - vz (float): not use
+                        - x (float): base  X，站立时X位置
+                        - y (float): base  Y，站立时Y位置
+                        - z (float): base  Z，站立时Z位置
+                    - fsmstatename (dict): 有关状态机状态的数据
+                        - currentstatus (str): 当前状态 Unknown、Start、Zero、Stand、Walk、Stop
+                    - jointStates (list): 关节状态列表
+                        - name (str): 关节名称
+                        - qa (float): 真实的关节角度，单位：rad（弧度）
+                        - qdota (float): 真实的关节速度，单位：rad/s（弧度/秒）
+                        - taua (float): 真实的扭矩，单位:n*m
+                        - qc (float): 期望的关节速度，单位：rad
+                        - qdotc (float): 期望的关节速度，单位：rad/s（弧度/秒）
+                        - tauc (float): 期望的关节扭矩，单位：unit:n*m
+                    - stanceindex (dict): 姿态索引 not use
+                    - contactforce (dict): 接触力数据 not use
+                        - fxL (float): 左脚接触力
+                        - fyL (float): 左脚接触力
+                        - fzL (float): 左脚接触力
+                        - mxL (float): 左脚接触力
+                        - myL (float): 左脚接触力
+                        - mzL (float): 左脚接触力
+                        - fxR (float): 右脚接触力
+                        - fyR (float): 右脚接触力
+                        - fzR (float): 右脚接触力
+                        - mxR (float): 右脚接触力
+                        - myR (float): 右脚接触力
+                        - mzR (float): 右脚接触力
+                - timestamp (dict): 时间戳
+                    - nanos (int):
+                    - seconds (str):
+            function (str): 接口名
+        Example:
+            {
+                "data": {
+                    "states": {
+                        "basestate": {
+                            "a": -0.00008816774229518624,
+                            "b": -0.0031777816310660227,
+                            "c": 0,
+                            "va": -3.2955695877132929e-9,
+                            "vb": -6.542262024864478e-7,
+                            "vc": 2.0403557796187139e-8,
+                            "vx": 0,
+                            "vy": 0,
+                            "vz": 0,
+                            "x": 0,
+                            "y": 0,
+                            "z": 0
+                        },
+                        "contactforce": {
+                            "fxL": 0,
+                            "fxR": 6,
+                            "fyL": 1,
+                            "fyR": 7,
+                            "fzL": 2,
+                            "fzR": 8,
+                            "mxL": 3,
+                            "mxR": 9,
+                            "myL": 4,
+                            "myR": 10,
+                            "mzL": 5,
+                            "mzR": 11
+                        },
+                        "fsmstatename": {
+                            "currentstatus": "Start"
+                        },
+                        "jointStates": [
+                            {
+                                "name": "left_hip_roll",
+                                "qa": -0.000002967348844382189,
+                                "qc": -4.195799309522971e-9,
+                                "qdota": -1.2811068419807388e-8,
+                                "qdotc": -2.5650460977039419e-9,
+                                "taua": 0.00000421397498061693,
+                                "tauc": 0.00000421397498061693
+                            },
+                            {
+                                "name": "left_hip_yaw",
+                                "qa": 1.1561011056000389e-7,
+                                "qc": 5.763118985802831e-10,
+                                "qdota": 5.413053331490085e-10,
+                                "qdotc": -1.998095673038479e-9,
+                                "taua": -5.607576848879348e-7,
+                                "tauc": -5.607576848879348e-7
+                            },
+                            {
+                                "name": "left_hip_pitch",
+                                "qa": 0.00004391517501779261,
+                                "qc": 1.515751869369811e-8,
+                                "qdota": 1.9014878092501132e-7,
+                                "qdotc": -4.227869290635517e-8,
+                                "taua": -0.000007239519592483131,
+                                "tauc": -0.000007239519592483131
+                            },
+                            {
+                                "name": "left_knee_pitch",
+                                "qa": 0.00004577103623661791,
+                                "qc": 1.825644254205245e-8,
+                                "qdota": 1.9871683938840232e-7,
+                                "qdotc": -1.3400628221563269e-7,
+                                "taua": -0.000004188456587918816,
+                                "tauc": -0.000004188456587918816
+                            },
+                            {
+                                "name": "left_ankle_pitch",
+                                "qa": 0.0000515945298803933,
+                                "qc": 2.2981673142499234e-8,
+                                "qdota": 2.242746827673787e-7,
+                                "qdotc": -2.258893072672217e-7,
+                                "taua": -7.153918887352573e-8,
+                                "tauc": -7.153918887352573e-8
+                            },
+                            {
+                                "name": "left_ankle_roll",
+                                "qa": 6.419495520105573e-7,
+                                "qc": 3.706374175342285e-11,
+                                "qdota": 2.794181899265958e-9,
+                                "qdotc": -5.949285977052194e-9,
+                                "taua": 1.093729550329863e-10,
+                                "tauc": 1.093729550329863e-10
+                            },
+                            {
+                                "name": "right_hip_roll",
+                                "qa": 0.0000028389355052439439,
+                                "qc": 4.865708590789946e-9,
+                                "qdota": 1.2246925191446977e-8,
+                                "qdotc": -3.962174546204988e-9,
+                                "taua": -0.000004837825973754749,
+                                "tauc": -0.000004837825973754749
+                            },
+                            {
+                                "name": "right_hip_yaw",
+                                "qa": -4.364693140246345e-7,
+                                "qc": 6.000702384094449e-10,
+                                "qdota": -1.8497568931031923e-9,
+                                "qdotc": -1.7781221204499439e-9,
+                                "taua": -5.867529228984824e-7,
+                                "tauc": -5.867529228984824e-7
+                            },
+                            {
+                                "name": "right_hip_pitch",
+                                "qa": 0.000045113585488131829,
+                                "qc": 2.367752787246051e-8,
+                                "qdota": 1.950714297088208e-7,
+                                "qdotc": -6.520824184784889e-8,
+                                "taua": -0.000011320537478692172,
+                                "tauc": -0.000011320537478692172
+                            },
+                            {
+                                "name": "right_knee_pitch",
+                                "qa": 0.0000479437468878189,
+                                "qc": 2.324249646390596e-8,
+                                "qdota": 2.0757655546078694e-7,
+                                "qdotc": -1.4486023522267125e-7,
+                                "taua": -0.00000557281564261239,
+                                "tauc": -0.00000557281564261239
+                            },
+                            {
+                                "name": "right_ankle_pitch",
+                                "qa": 0.00005468652781599774,
+                                "qc": 2.4630029782206445e-8,
+                                "qdota": 2.3684484798495586e-7,
+                                "qdotc": -2.2533190930925487e-7,
+                                "taua": -7.817536142908409e-8,
+                                "tauc": -7.817536142908409e-8
+                            },
+                            {
+                                "name": "right_ankle_roll",
+                                "qa": -1.4411157156501987e-7,
+                                "qc": 8.786951464767337e-11,
+                                "qdota": -6.347293532005193e-10,
+                                "qdotc": -6.275949957243541e-9,
+                                "taua": 5.977234519649815e-11,
+                                "tauc": 5.977234519649815e-11
+                            },
+                            {
+                                "name": "waist_yaw",
+                                "qa": 2.7287197903010758e-10,
+                                "qc": -1.9509172839224989e-10,
+                                "qdota": 2.182983232727597e-7,
+                                "qdotc": -1.5630533392766103e-7,
+                                "taua": -0.000003249343357926737,
+                                "tauc": -0.0000017639729379187398
+                            },
+                            {
+                                "name": "waist_pitch",
+                                "qa": -1.1411541437762108e-8,
+                                "qc": -5.783273072262379e-9,
+                                "qdota": -5.121972652033971e-13,
+                                "qdotc": 3.810219915783962e-8,
+                                "taua": 0.000011505459672511687,
+                                "tauc": 0.000005496170595926694
+                            },
+                            {
+                                "name": "waist_roll",
+                                "qa": -1.302909426086466e-8,
+                                "qc": -6.480917136286735e-9,
+                                "qdota": -3.6044103175709825e-13,
+                                "qdotc": -4.3982596326637839e-10,
+                                "taua": 0.000013027709577777855,
+                                "tauc": 0.000006483935166648911
+                            },
+                            {
+                                "name": "head_yaw",
+                                "qa": 0,
+                                "qc": 0,
+                                "qdota": 0,
+                                "qdotc": 0,
+                                "taua": 0,
+                                "tauc": 0
+                            },
+                            {
+                                "name": "head_pitch",
+                                "qa": 0,
+                                "qc": 0,
+                                "qdota": 0,
+                                "qdotc": 0,
+                                "taua": 0,
+                                "tauc": 0
+                            },
+                            {
+                                "name": "head_roll",
+                                "qa": 0,
+                                "qc": 0,
+                                "qdota": 0,
+                                "qdotc": 0,
+                                "taua": 0,
+                                "tauc": 0
+                            }
+                        ],
+                        "stanceindex": {}
+                    },
+                    "timestamp": {
+                        "nanos": 2,
+                        "seconds": "1"
+                    }
+                },
+                "function": "SonnieGetStates"
+            }
         """
         response = requests.get(f'{self._baseurl}/robot/enable_states_listen')
         return response.json()
